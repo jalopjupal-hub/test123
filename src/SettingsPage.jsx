5f9d2989d890db38030cfe08eba78e1b26c88901
jalopjupal-hub <jalopjupal@gmail.com>
2026-07-06 09:55:39 +0800

    Create SettingsPage.jsx

---
+++ src/SettingsPage.jsx
@@ -0,0 +1,9 @@
+export default function SettingsPage() {
+  return (
+    <div className="settings-page">
+      <header className="settings-header">
+        <h1>Settings</h1>
+      </header>
+    </div>
+  );
+}
